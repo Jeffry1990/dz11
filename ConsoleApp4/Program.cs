54dc583963ffe2a43b5fb6f8e3ce1bce295fd296
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-13 07:56:10 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "qwerty";
+            string userInput;
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+                if (password == userInput)
+                {
+                    Console.WriteLine("Тайное сообщение прочитано");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверно, введите пароль еще раз");
+                }
+            }
+        }
+    }
+}
